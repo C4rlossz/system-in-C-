dc7296d21beb71eda9f8e590f8fddac9ea343ff3
Carlos <carlosz.eduardo1402@gmail.com>
2025-01-08 13:34:51 -0300

    blog system in C#

---
+++ Models/Role.cs
@@ -0,0 +1,13 @@
+using Dapper.Contrib.Extensions;
+
+namespace Blog.Models
+{
+    [Table("[Role]")]
+        public class Role
+        {
+
+            public int Id { get; set; }
+            public required string Name { get; set; }
+            public required string Slug { get; set; }
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Blog.Models;
+using Blog.Repositories;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+
+
+namespace Blog
+{
+
+    class Program
+    {
+
+        private const string CONNECTION_STING = "Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True;";
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(CONNECTION_STING);
+            connection.Open();
+
+            ReadUsers(connection);
+            ReadRoles(connection);
+
+
+            connection.Close();
+        }
+        public static void ReadUsers(SqlConnection connection)
+        {
+            var repository = new UserRepository(connection);
+            var users = repository.Get();
+
+            foreach (var user in users)
+                Console.WriteLine(user.Name);
+
+        }
+
+                public static void ReadRoles(SqlConnection connection)
+        {
+            var repository = new RoleRepository(connection);
+            var roles = repository.Get();
+
+            foreach (var role in roles)
+                Console.WriteLine(role.Name);
+        }
+    }
+}
